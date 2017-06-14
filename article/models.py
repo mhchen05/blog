822dc62d92d1fee51bb7afd0a1106cd78351a3fb
allen <allen@localhost.localdomain>
2017-06-14 16:55:12 +0800

    django tutorial init

---
+++ article/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100) # 博客题目
+    category = models.CharField(max_length = 50) # 博客标签
+    date_time = models.DateTimeField(auto_now_add = True) # 博客日期
+    content = models.TextField(blank = True,null = True) # 博客文章正文
+
+    def __str__(self):
+        return self.title
+
+    # 按时间降序排序
+    class Meta:
+        ordering = ['-date_time']
+
